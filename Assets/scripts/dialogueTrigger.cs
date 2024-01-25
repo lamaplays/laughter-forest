bc4f3a75badbb27fc80ac1623348598b8432cd67
lamaplays <lamah2003@gmail.com>
2024-01-25 10:14:33 +0300

    working on the dialogue

---
+++ Assets/scripts/dialogueTrigger.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class dialogueTrigger : MonoBehaviour
+{
+    [SerializeField] private List<dialogueString> dialoguStrings = new List<dialogueString> ();
+    [SerializeField] private Transform NPCTransform;
+
+    private bool hasSpoken  = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")&& !hasSpoken)
+        {
+            other.gameObject.GetComponent<dialogueManager>()DialogueStart(dialoguStrings, NPCTransform);
+            hasSpoken = true;   
+        }
+    }
+}
+[System.Serializable]
+public class dialogueString
+{
+    public string text;
+    public bool isEnd;
+
+    [Header("Brach")]
+    public bool isQuestion;
+    public string answerOption1;
+    public string answerOption2;
+    public int option1IndexJump;
+    public int option2IndexJump;
+
+    [Header("Triggered Events")]
+    public UnityEvent startDialogueEvent;
+    public UnityEvent endDialogueEvent;
+}
+
